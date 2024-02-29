ada253c22a3a7a8083600465e7303fe69ca8fb94
Lak1n26 <ldvrn01@gmail.com>
2024-02-29 19:33:47 +0300

    Переделал методику Плана

--- finance-tracker-streamlit.py
+++ finance-tracker-streamlit.py
@@ -9,9 +9,13 @@ import os
 API_KEY = os.environ['API_KEY']
 SHEET_ID = os.environ['SHEET_ID']
 
+
+st.title('Finance Tracker App')
+
 sheet_name = 'Факт'
 range = 'A1:E1000'
 
+# подтягиваем фактические данные из Google Sheets
 url_data = requests.get(f'https://sheets.googleapis.com/v4/spreadsheets/{SHEET_ID}/values/{sheet_name}!{range}?key={API_KEY}')
 df = pd.DataFrame(url_data.json()['values'][1:], columns=url_data.json()['values'][0])
 df['Дата'] = pd.to_datetime(df['Дата'], format='%d.%m.%Y')
@@ -19,11 +23,9 @@ df = df[df['Дата'].notna()]
 for col in ['На карте + нал', 'На вкладах', 'Инвестиции', 'Всего']:
     df[col] = df[col].str.split().str.join('').astype(int)
 
-st.title('Finance Tracker App')
-
 year = st.selectbox(
-    'Select a year',
-    (2023, 2024),  index=None)
+    'Выберите год',
+    (2023, 2024),  index=None, placeholder='Выберите значение')
 
 if year is not None:
     uniq_months = df[df['Дата'].dt.year == year]['Дата'].dt.month.unique()
@@ -31,8 +33,8 @@ else:
     uniq_months = df['Дата'].dt.month.unique()
 
 months = st.multiselect(
-    'Select the month(s)',
-    tuple(uniq_months))
+    'Выберите месяц(ы)',
+    tuple(uniq_months), placeholder='Выберите значения')
 
 
 if year is not None and months != []:
@@ -43,7 +45,7 @@ elif months != []:
     part = df[(df['Дата'].dt.month.isin(months))]
 else:
     part = df
-st.table(part.reset_index(drop=True))
+st.dataframe(part.reset_index(drop=True))
 st.line_chart(data=part, x='Дата', y='Всего')
 
 last_change = str(max(df['Дата']))[:10]
@@ -54,7 +56,7 @@ st.text(f'Дата последнего внесения данных: {last_cha
 st.title('Линейная регрессия')
 from sklearn.linear_model import LinearRegression
 from datetime import timedelta
-
+df = df[df['Дата'] >= '2023-10-01']
 periods = st.number_input("Введите прогнозируемое число дней", value=30)
 
 date_range = pd.DataFrame(pd.date_range(min(df['Дата']), max(df['Дата'])), columns=['Дата'])
@@ -68,9 +70,8 @@ y = train_df['Всего']
 reg = LinearRegression(fit_intercept=True).fit(X, y)
 
 st.write(f'Прогнозируемые сбережения за 1 день: {int(reg.coef_[0])} рублей')
-
 economy_per_day = st.slider('Величина сбережений за 1 день', int(reg.coef_[0]) // 2, int(reg.coef_[0]) * 2, int(reg.coef_[0]))
-
+st.write(f'Прогнозируемые сбережения за месяц: {int(economy_per_day * 30)} руб.')
 train_df['Прогноз'] = reg.intercept_ + train_df.index * economy_per_day
 
 # прогнозируем на N дней вперед
@@ -83,25 +84,25 @@ total_df = pd.concat([train_df, test_df], axis=0)
 st.line_chart(data=total_df, x='Дата', y=['Всего', 'Прогноз'], color=[(0, 0, 255), (255, 0, 0)])
 
 
-# создадим ДФ с отчетными датами
-plan_completed = pd.DataFrame({'Дата': ['2023-12-31', '2024-01-31', '2024-02-29', '2024-03-31', '2024-04-30', '2024-05-31', '2024-06-30', '2024-07-31', '2024-08-31'],
-                               'Период': ['На конец декабря \'23', 'На конец января \'24', 'На конец февраля \'24', 'На конец марта \'24', 'На конец апреля \'24', 
-                                'На конец мая \'24', 'На конец июня \'24', 'На конец июля \'24', 'На конец августа \'24'],
-                                'План, тыс.руб.': [150, 220, 410, 470, 540, 620, 700, 800, 850]})
+# подтягиваем План из Google Sheets
+sheet_name = 'План'
+range = 'A1:C20'
+url_data = requests.get(f'https://sheets.googleapis.com/v4/spreadsheets/{SHEET_ID}/values/{sheet_name}!{range}?key={API_KEY}')
+plan_completed = pd.DataFrame(url_data.json()['values'][1:], columns=url_data.json()['values'][0])
 plan_completed['Дата'] = pd.to_datetime(plan_completed['Дата'])
-
+plan_completed['План, тыс. руб.'] = plan_completed['План, тыс. руб.'].astype(np.int64)
 # прогноз сбережений на год вперед
 test_df = pd.DataFrame(pd.date_range(max(train_df['Дата']) + timedelta(days=1), periods=365), columns=['Дата'])
 test_df.index = np.arange(max(train_df.index) + 1, max(train_df.index) + 365 + 1)
 test_df['Прогноз'] = reg.intercept_ + test_df.index * economy_per_day
 total_df = pd.concat([train_df, test_df], axis=0)
 
-# оставляем прогнозируемые значения на отчетные даты
+# # оставляем прогнозируемые значения на отчетные даты
 plan_completed = plan_completed.merge(total_df)
-plan_completed['Факт, тыс.руб.'] = plan_completed['Всего'].fillna(plan_completed['Прогноз'])
-plan_completed['Факт, тыс.руб.'] = (plan_completed['Факт, тыс.руб.'] // 1000).astype(np.int64)
-plan_completed = plan_completed[['Период', 'План, тыс.руб.', 'Факт, тыс.руб.']]
-plan_completed['Выполнен план'] = (plan_completed['План, тыс.руб.'] <= plan_completed['Факт, тыс.руб.'])
+plan_completed['Факт, тыс. руб.'] = plan_completed['Всего'].fillna(plan_completed['Прогноз'])
+plan_completed['Факт, тыс. руб.'] = (plan_completed['Факт, тыс. руб.'] // 1000).astype(np.int64)
+plan_completed = plan_completed[['Период', 'План, тыс. руб.', 'Факт, тыс. руб.']]
+plan_completed['Выполнен план'] = (plan_completed['План, тыс. руб.'] <= plan_completed['Факт, тыс. руб.'])
 plan_completed['Выполнен план'] = plan_completed['Выполнен план'].apply(lambda x: 'Да' if x == True else 'Нет')
 
 
